2eee4784b2e508a3ebc5d3f5b8b5e143ac080d42
Henrik Hamilton <henrikham14@gmail.com>
2024-05-28 18:25:14 -0400

    5.28

--- solutions.py
+++ solutions.py
@@ -11,6 +11,13 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+    sales = input("What is the company's annual total sales figure? ")
+    profitpt1 = int(sales)*23
+    totalprofit = float(profitpt1 / 100)
+    formattedprofit = "Profit: " + "$" + "{0:,.2f}".format(totalprofit)
+    print(formattedprofit)
+
+
 
 
 def calculate_quotient_and_remainder():
@@ -22,6 +29,15 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
+    n1 = int(input("enter one integer: "))
+    n2 = int(input("enter another integer: "))
+    n3 = float(n1 / n2)
+    truncatedn3 = ("{0:.0f}".format(n3))
+    remainder = n1 % n2
+    if int(truncatedn3) != 1: 
+      print(str(n2) + " goes into " + str(n1) + " a total of " + str(truncatedn3) + " times with a remainder of " + str(remainder))
+    if int(truncatedn3) == 1: 
+      print(str(n2) + " goes into " + str(n1) + " a total of " + str(truncatedn3) + " time with a remainder of " + str(remainder))
 
 
 def calculate_miles_per_gallon():
@@ -35,6 +51,12 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
+    miles_driven = input("how many miles have been driven? ")
+    gas_used = input("how many gallons of gas have been used? ")
+    mpg = float(miles_driven) / float(gas_used)
+    print("Miles driven: " + str(miles_driven) + "\n" + "Gas used (gallons): " + str(gas_used) + "\n" + "Miles per gallon: " + str(mpg))
+
+
 
 
 def align_text():
@@ -53,3 +75,14 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    price1 = float(input("Enter price #1: "))
+    price2 = float(input("Enter price #2: "))
+    price3 = float(input("Enter price #3: "))
+    formattedprice1 = "{0:.2f}".format(price1)
+    formattedprice2 = "{0:.2f}".format(price2)
+    formattedprice3 = "{0:.2f}".format(price3)
+    ralignedprice1 = format(formattedprice1, ">10s")
+    ralignedprice2 = format(formattedprice2, ">10s")
+    ralignedprice3 = format(formattedprice3, ">10s")
+    print("Here are your prices!" + "\n")
+    print("Price #1: $ " + str(ralignedprice1) + "\n" + "Price #2: $ " + str(ralignedprice2) + "\n" + "Price #3: $ " + str(ralignedprice3))
